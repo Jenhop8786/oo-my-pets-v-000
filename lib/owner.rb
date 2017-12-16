503bb08cdb4130f9fa70cfaf7431ed2d79c7b092
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 23:45:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,7 +72,6 @@ def sell_pets
 end
 
 def list_pets
-  "I have #{pets[:fishes].count} fish, #{pets[:dogs].count}
-  dog(s), and #{pets[:cats].count} cat(s)."
+  "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
 end
-end 
+end
